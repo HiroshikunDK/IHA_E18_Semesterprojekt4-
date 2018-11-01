a9c210b7a3695c047c57fe900869c9ae121d9abb
Dennis Bülow Jørgensen <201508948@post.au.dk>
2018-11-01 14:01:10 +0100

    CHanged soem code to the views

--- VikingNotes/View/Views/MakeNewQuizView.xaml.cs
+++ VikingNotes/View/Views/MakeNewQuizView.xaml.cs
@@ -12,23 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ViewModels;
 
 namespace View.Views
 {
     /// <summary>
-    /// Interaction logic for MakeQuizView.xaml
+    /// Interaction logic for MakeNewQuizView.xaml
     /// </summary>
-    public partial class MakeQuizView : UserControl
+    public partial class MakeNewQuizView : UserControl
     {
-        public MakeQuizView()
+        public MakeNewQuizView()
         {
             InitializeComponent();
-        }
-
-        private void btn_makeNewQuiz_Clicked(object sender, RoutedEventArgs e)
-        {
-            DataContext = new MakeNewQuizViewModel();
         }
     }
 }
