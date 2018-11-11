c4619df3a28985bf67644920f962f7d320745aec
Virkman <nvirkman@gmail.com>
2018-11-11 23:55:08 +0100

    Need to be reverted

--- VikingNotes/DAL/Core/IUnitOfWork.cs
+++ VikingNotes/DAL/Core/IUnitOfWork.cs
@@ -4,21 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL.Core.Repositories;
+using RESTfullWebApi.Models;
 
 namespace DAL.Core
 {
     public interface IUnitOfWork
     {
         IUserRepository User { get; }
-        IAnswerRepository Answer { get; }
-        ICatagoryRepository Catagory { get; }
-        IQuestionRepository Question { get; }
-        IQuizRepository Quiz { get; }
-        IStudyRepository Study { get; }
-        IUserTypeRepository UserType { get; }
-        ICourseRepository Course { get;  }
-        IFacultyRepository Faculty { get; }
-        IRatingRepository Rating { get; }
-        ISemesterRepository Semester { get; }
+        IRepository<Answer> Answer { get; }
+        IRepository<Catagory> Catagory { get; }
+        IRepository<Question> Question { get; }
+        IRepository<Quiz> Quiz { get; }
+        IRepository<Study> Study { get; }
+        IRepository<UserType> UserType { get; }
+        IRepository<Course> Course { get; }
+        IRepository<Faculty> Faculty { get; }
+        IRepository<Rating> Rating { get; }
+        IRepository<Semester> Semester { get; }
     }
 }
