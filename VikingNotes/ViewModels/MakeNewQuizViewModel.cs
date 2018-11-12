bae72b29aa0f34531d87897866f3d9c0a2f1ba26
Dennis Bülow Jørgensen <201508948@post.au.dk>
2018-11-12 14:05:05 +0100

    Revert "skal revertes"
    This reverts commit 9a416a6cdbe4d375f4db73c9ca231bf4633231c7.

--- VikingNotes/ViewModels/MakeNewQuizViewModel.cs
+++ VikingNotes/ViewModels/MakeNewQuizViewModel.cs
@@ -3,24 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using ViewModels.Commands;
 
 namespace ViewModels
 {
     public class MakeNewQuizViewModel : BaseViewModel
     {
-        private string Question = "";
-        private string MCQName = "";
-        private List<string> AnswerPossibilities;
-        private List<bool> Correctness;
-
-
-        public MakeNewQuizViewModel()
-        {
-
-        }
-
 
     }
 }
