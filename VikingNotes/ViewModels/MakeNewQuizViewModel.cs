163ad11e841ee5b01df5d16747f76689c6f46aeb
Dennis Bülow Jørgensen <201508948@post.au.dk>
2018-11-12 15:32:28 +0100

    Added Grids to VIew Make New Quiz

--- VikingNotes/ViewModels/MakeNewQuizViewModel.cs
+++ VikingNotes/ViewModels/MakeNewQuizViewModel.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ViewModels.Commands;
 
 namespace ViewModels
 {
     public class MakeNewQuizViewModel : BaseViewModel
     {
+        private string quizName { get; set; }
 
+        public string QuizName
+        {
+            get { return quizName; }
+            set { quizName = value; }
+        }
+
+        public ICommand GemogNaeste { get; set; }
+
+        public MakeNewQuizViewModel()
+        {
+            GemogNaeste = new Command(SOmething,CanExecute);
+        }
+
+
+        private bool CanExecute(object o)
+        {
+            return true;
+        }
+
+        private void SOmething(object o)
+        {
+            MessageBox.Show("Gemtaf");
+        }
+        
     }
 }
