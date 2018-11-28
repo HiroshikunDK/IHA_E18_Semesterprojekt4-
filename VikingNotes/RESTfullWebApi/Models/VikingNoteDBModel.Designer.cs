9d3918eeb4634286e7e93c4a0351b9050b54b2d9
Virkman <nvirkman@gmail.com>
2018-11-28 01:19:19 +0100

    Addet nye models, henter users study og faculty ind når der laves quiz

--- VikingNotes/RESTfullWebApi/Models/VikingNoteDBModel.Designer.cs
+++ VikingNotes/RESTfullWebApi/Models/VikingNoteDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Virkman\source\repos\IHA_E18_Semesterprojekt4-\VikingNotes\RESTfullWebApi\Models\VikingNoteDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Virkman\Source\Repos\IHA_E18_Semesterprojekt4-\VikingNotes\RESTfullWebApi\Models\VikingNoteDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
